56075495fd1642ef0f02bba72c554983605672a9
caleb-007 <calebareeveso7@gmail.com>
2021-01-07 10:28:40 +0100

    nav clean up

--- src/component/nav.js
+++ src/component/nav.js
@@ -41,29 +41,7 @@ export default class nav extends Component {
                   <div>
                 <Link to='/'>  <img src={Logo} alt=""></img></Link>
                   </div>
-                {/* <IconButton   color="primary"  aria-label="Login" style={{outline:"none"}}>
-                 <Link to='/login'  className='px-3 py-2 dot1 trans'><img src={Login} alt=''></img></Link>
-                </IconButton>
-
-                <IconButton  color="primary" aria-label="Home" style={{outline:"none"}}>
-                 <Link to='/'   className='dot2 py-1 trans'><img src={Home} alt=''></img></Link>
-                </IconButton>
-
-                <IconButton color="primary" aria-label="Privcy Policy" className='dot3' style={{outline:"none"}}>
-                 <Link to='/privacy-policy' className='px-2 trans' style={{padding:"0em 0.4em 0.1em 0.4em"}}><img src={Privacy} alt=''></img></Link>
-                </IconButton> */}
-                {/* <div className=" ml-auto d-flex align-items-center ">
-                  <div className='pr-2'>
-                    <span className='dblue navt1'>Login</span>
-                    <span className='dblue navt2'>/</span>
-                    <span className='dblue navt3'>Signin</span>
-                  </div>
-                <div className="containerNav "  onClick={() => this.myFunctionNav()}>
-                                <div className="bar1"></div>
-                                <div className="bar3"></div>
-
-                </div>
-                </div> */}
+              
             </nav>
         )
     }
