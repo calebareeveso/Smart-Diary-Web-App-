fa572b7aaa1b31dbc6743a90a32ab01c93298e86
caleb-007 <calebareeveso7@gmail.com>
2021-01-10 16:46:04 +0100

    made instantredirecthome page

---
+++ src/component/instantredirecthome.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+// react router 
+
+import {Redirect,withRouter } from 'react-router-dom';
+
+// css 
+import './assets/css/loader.css'
+
+
+ class instantredirecthome extends Component {
+
+
+    constructor(){
+        super()
+
+        this.state=({
+            redirect: false,
+            progress: 0
+        })
+        this.redirection = this.redirection.bind(this)
+    }
+
+
+    
+     
+
+
+    componentDidMount(){
+        this.redirection()
+    }
+
+
+    redirection(){
+        setTimeout(() => {
+            this.setState({redirect: true})
+            
+        }, 50);
+    }
+    render() {
+
+
+        return (
+            <div >
+                {this.state.redirect ? 
+                <Redirect to={`diary#${localStorage.getItem('DiaryName')}`} />
+                 :
+                 null
+            }
+
+           <div>
+
+               {/* empty */}
+
+           </div>
+           </div>
+            
+        )
+    }
+}
+
+export default withRouter(instantredirecthome); 
